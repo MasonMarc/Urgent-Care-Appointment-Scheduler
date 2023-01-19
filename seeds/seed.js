6b0d93bc30bc0a4fab0bffd03d70bc5951bcf3f5
MasonMarc <masonmarcantel@gmail.com>
2023-01-19 11:02:12 -0500

    added seed

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,7 +1,16 @@
 const sequelize = require('../config/connection');
+const userData = require('./user_seeds.json');
+const User = require('../models/User');
+
+
 
 const init = async () => {
   await sequelize.sync({ force: true });
+
+  await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
   process.exit(0);
 };
 
