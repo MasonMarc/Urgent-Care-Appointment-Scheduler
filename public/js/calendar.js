2104d0d5fdccd27f6317d011da4e00c3d30092bd
ashley <alandis01@gmail.com>
2023-01-20 14:38:18 -0500

    added days of week and business hours to calendar

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -22,9 +22,28 @@ document.addEventListener('DOMContentLoaded', function () {
                 // end: '2023-01-02' // a property! ** see important note below about 'end' **
             }
         ],
+        businessHours: {
+            // days of week. an array of zero-based day of week integers (0=Sunday)
+            daysOfWeek: [ 1, 2, 3, 4, 5, 6, 7], // Monday - Thursday
+          
+            startTime: '08:00', // a start time (10am in this example)
+            endTime: '20:00', // an end time (6pm in this example)
+          },
         eventDidMount: (info) => {
             console.log(info);
         }
     });
+        // var calendar = new Calendar(calendarEl, {
+
+        //     eventClick: function(info) {
+        //       alert('Event: ' + info.event.title);
+        //       alert('Coordinates: ' + info.jsEvent.pageX + ',' + info.jsEvent.pageY);
+        //       alert('View: ' + info.view.type);
+          
+        //       // change the border color just for fun
+        //       info.el.style.borderColor = 'red';
+        //     }
+        //   });
+ 
     calendar.render();
 });
