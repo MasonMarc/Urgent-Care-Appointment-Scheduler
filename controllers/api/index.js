7929d768a10e43f5eeb86653fdd1311ff8108250
MasonMarc <masonmarcantel@gmail.com>
2023-01-20 11:23:59 -0500

    fixed services routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes.js');
-
 router.use('/users', userRoutes);
+
+const serviceRoutes = require('./service-routes.js');
+router.use('/services', serviceRoutes);
 
 module.exports = router;
--- controllers/api/service-routes.js
+++ controllers/api/service-routes.js
@@ -1,18 +1,11 @@
 const router = require('express').Router();
 const { Service } = require('../../models');
 
-  router.get('/services', async (req, res) => {
-    try {
-      const dbServiceData = await Service.findAll(req.body);
-  
-      const service = dbServiceData.get({ plain: true });
-      res.render('services', { service });
-      console.log(service);
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-    }
+router.get('/', (req, res) => {
+  Service.findAll().then((serviceData) => {
+    res.json(serviceData);
   });
+});
 
 
 
