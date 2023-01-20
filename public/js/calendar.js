5ae8919b4c41bd1a41ce31a6f4417a233003e014
ashley <alandis01@gmail.com>
2023-01-20 14:19:45 -0500

    removed second calendar script from main handlebars

--- public/js/calendar.js
+++ public/js/calendar.js
@@ -1,23 +1,30 @@
 document.addEventListener('DOMContentLoaded', function () {
     var calendarEl = document.getElementById('calendar');
     var calendar = new FullCalendar.Calendar(calendarEl, {
-        initialView: 'dayGridMonth',
+        timeZone: 'UTC',
         events: [
             { // this object will be "parsed" into an Event Object
+                id: 1,
                 title: 'The Title', // a property!
                 start: '2023-01-01', // a property!
-                end: '2023-01-02' // a property! ** see important note below about 'end' **
+                // end: '2023-01-02' // a property! ** see important note below about 'end' **
+            },
+            { // this object will be "parsed" into an Event Object
+                id: 2,
+                title: 'The Title', // a property!
+                start: '2023-01-03', // a property!
+                // end: '2023-01-02' // a property! ** see important note below about 'end' **
+            },
+            { // this object will be "parsed" into an Event Object
+                id: 3,
+                title: 'The Title', // a property!
+                start: '2023-01-04', // a property!
+                // end: '2023-01-02' // a property! ** see important note below about 'end' **
             }
         ],
-        dateClick: function () {
-            console.log("Test");
-        },
-        plugins: [
-        ],
-        selectable: true
+        eventDidMount: (info) => {
+            console.log(info);
+        }
     });
     calendar.render();
-    calendar.on("dateClick", (e) => {
-        console.log(e);
-    })
 });
