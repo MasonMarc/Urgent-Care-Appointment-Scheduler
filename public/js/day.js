9695ec2d061cc5a2333b5a11d9ab619dd49f7139
MasonMarc <masonmarcantel@gmail.com>
2023-01-23 14:18:32 -0500

    fixed profile page

---
+++ public/js/day.js
@@ -0,0 +1,11 @@
+const dayjs = require('dayjs')
+//import dayjs from 'dayjs' // ES 2015
+dayjs().format()
+
+let today = dayjs().format('YYYY-MM-DDTHH:MM:A');
+
+console.log(today);
+
+const current = () => {
+    return today;
+}
