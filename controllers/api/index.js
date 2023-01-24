3cba2964338c16615336b76cd1f00c8a013f7412
MasonMarc <masonmarcantel@gmail.com>
2023-01-24 12:36:47 -0500

    added privacy

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes.js');
+const withAuth = require('../../utils/auth');
 router.use('/users', userRoutes);
 
 const serviceRoutes = require('./service-routes.js');
-router.use('/services', serviceRoutes);
+router.use('/services', withAuth, serviceRoutes);
 
 const appointmentRoutes = require('./appointment-routes.js');
-router.use('/appointments', appointmentRoutes);
+router.use('/appointments', withAuth, appointmentRoutes);
 
 module.exports = router;
